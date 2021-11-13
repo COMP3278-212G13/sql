49d1591e288a3836a201d6935e5800719e317c48
Long-Kehan <80875468+Long-Kehan@users.noreply.github.com>
2021-11-13 14:11:35 +0800

    Add files via upload

--- facerecognition.sql
+++ facerecognition.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
---
+
 -- Host: localhost:3306
 -- Generation Time: Feb 17, 2020 at 09:41 PM
 -- Server version: 5.7.28-0ubuntu0.18.04.4
@@ -122,21 +122,30 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `Saving`;
 
 CREATE TABLE `Saving` (
-  `account_id` int NOT NULL,
+  `account_id` int(11) NOT NULL,
   `balance` float NOT NULL,
-  `intersest_rate%` float NOT NULL,
+  `interest_rate` float NOT NULL,
   `maturity_date` date NOT NULL,
-  PRIMARY KEY (account_id),
+  `value_date` date NOT NULL,
+  `saving_id` int(11) NOT NULL,
+  PRIMARY KEY (account_id, saving_id),
   FOREIGN KEY(account_id) REFERENCES Account(account_id)
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `Saving` WRITE;
 /*!40000 ALTER TABLE `Saving` DISABLE KEYS */;
-INSERT INTO `Saving` VALUES (1,20000,8,'2025-09-21');
-INSERT INTO `Saving` VALUES (4,3000,6,'2030-03-05');
-INSERT INTO `Saving` VALUES (5,3000,7.5,'2030-03-05');
-INSERT INTO `Saving` VALUES (10,100000,8.5,'2040-10-15');
-INSERT INTO `Saving` VALUES (14,10000,6,'2030-12-30');
+INSERT INTO `Saving` VALUES (1, 20000, 8, '2025-09-21', '2015-09-21', 1);
+INSERT INTO `Saving` VALUES (1, 20000, 7, '2022-03-11', '2021-03-11', 2);
+INSERT INTO `Saving` VALUES (1, 1000, 3, '2021-11-19', '2021-11-01', 3);
+INSERT INTO `Saving` VALUES (1, 2000, 3, '2021-11-20', '2021-11-03', 4);
+INSERT INTO `Saving` VALUES (1, 5000, 5, '2021-11-20', '2021-11-03', 5);
+INSERT INTO `Saving` VALUES (4, 3000, 6, '2030-03-05', '2020-03-05', 1);
+INSERT INTO `Saving` VALUES (5, 3000, 7.5, '2030-03-05', '2020-03-05', 1);
+INSERT INTO `Saving` VALUES (5, 1000, 3, '2021-11-19', '2021-11-09', 2);
+INSERT INTO `Saving` VALUES (5, 1000, 3, '2021-11-18', '2021-11-01', 3);
+INSERT INTO `Saving` VALUES (10, 100000, 8.5, '2040-10-15', '2020-10-15', 1);
+INSERT INTO `Saving` VALUES (14, 10000, 6, '2030-12-30', '2020-12-30', 1);
+
 /*!40000 ALTER TABLE `Saving` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,17 +155,26 @@ DROP TABLE IF EXISTS `Credit`;
 
 CREATE TABLE `Credit` (
   `account_id` int NOT NULL,
+  `month` int NOT NULL,
   `bill` float NOT NULL,
   `due_date` date NOT NULL,
-  PRIMARY KEY (account_id),
+  `repay_date` date DEFAULT NULL,
+  PRIMARY KEY (account_id, month),
   FOREIGN KEY(account_id) REFERENCES Account(account_id)
 ) ;
 
 LOCK TABLES `Credit` WRITE;
 /*!40000 ALTER TABLE `Credit` DISABLE KEYS */;
-INSERT INTO `Credit` VALUES (6,50000,'2021-12-01');
-INSERT INTO `Credit` VALUES (9,100000,'2022-12-01');
-INSERT INTO `Credit` VALUES (13,30000,'2023-12-01');
+INSERT INTO `Credit` VALUES (6, 202106, 10030,'2021-07-20','2021-07-19');
+INSERT INTO `Credit` VALUES (6, 202107, 2345,'2021-08-20','2021-08-15');
+INSERT INTO `Credit` VALUES (6, 202108, 3200, '2021-09-23', '2021-09-15');
+INSERT INTO `Credit` VALUES (6, 202109, 50000, '2021-10-23', '2021-10-20');
+INSERT INTO `Credit` VALUES (6, 202110, 2536, '2021-11-23', NULL);
+INSERT INTO `Credit` VALUES (9, 202108, 100000, '2021-09-20', '2021-10-10');
+INSERT INTO `Credit` VALUES (9, 202109, 2000, '2022-10-20','2021-10-14');
+INSERT INTO `Credit` VALUES (9, 202110, 500,'2021-11-20',NULL);
+INSERT INTO `Credit` VALUES (13, 202110, 30000, '2021-11-25', NULL);
+
 /*!40000 ALTER TABLE `Credit` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -164,19 +182,20 @@ UNLOCK TABLES;
 # Create TABLE 'Transaction'
 DROP TABLE IF EXISTS `Transaction`;
 
+
 CREATE TABLE `Transaction` (
-  `transaction_id` int NOT NULL auto_increment,
-  `from_account` int NOT NULL,
-  `to_account` int NOT NULL,
-  `due_date` date NOT NULL,
-  `amount` decimal NOT NULL,
-  `transac_date` date NOT NULL,
-  `transac_time` time NOT NULL,
+  `transaction_id` int(11) NOT NULL auto_increment,
+  `from_account` int(11) NOT NULL,
+  `to_account` int(11) NOT NULL,
+  `amount` decimal(10,0) NOT NULL,
+  `transac_datetime` datetime NOT NULL,
+  `from_balance` float NOT NULL,
+  `to_balance` float NOT NULL,
   `message` varchar(50) NOT NULL,
   PRIMARY KEY (transaction_id),
   FOREIGN KEY(from_account) REFERENCES Account(account_id),
   FOREIGN KEY(to_account) REFERENCES Account(account_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 auto_increment=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 set foreign_key_checks=0;
 truncate table Transaction;
@@ -184,7 +203,16 @@ set foreign_key_checks=1;
 
 LOCK TABLES `Transaction` WRITE;
 /*!40000 ALTER TABLE `Transaction` DISABLE KEYS */;
-# INSERT INTO `Transaction` VALUES();
+INSERT INTO `Transaction` VALUES(null, 8, 2, '500', '2021-08-28 11:53:56', 3000, 23900, 'test');
+INSERT INTO `Transaction` VALUES(null, 7, 8, '1000', '2021-09-15 10:45:23', 600, 4000, '3278bravo');
+INSERT INTO `Transaction` VALUES(null, 7, 2, '100', '2021-09-26 13:24:26', 500, 24000, '');
+INSERT INTO `Transaction` VALUES(null, 2, 7, '5000', '2021-10-18 11:23:54', 19000, 5500, 'group project nice');
+INSERT INTO `Transaction` VALUES(null, 2, 7, '3000', '2021-10-28 18:23:11', 16000, 8500, 'hello');
+INSERT INTO `Transaction` VALUES(null, 2, 8, '1000','2021-11-02 12:33:12', 15000, 5000, 'love mySQL');
+INSERT INTO `Transaction` VALUES(null, 8, 7, '500', '2021-11-05 17:03:27', 4500, 9000, 'Dr.Luo is awesome');
+INSERT INTO `Transaction` VALUES(null, 7, 2, '800', '2021-11-09 20:11:04', 8200, 15800, 'brilliant tutors');
+INSERT INTO `Transaction` VALUES(null, 11, 2, '100', '2021-11-10 20:11:04', 500, 15900, 'brilliant tutors');
+INSERT INTO `Transaction` VALUES(null, 2, 12, '1000', '2021-11-13 20:11:04', 14900, 11000, '');
 /*!40000 ALTER TABLE `Transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
